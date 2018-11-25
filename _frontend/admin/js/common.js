d3b4cf11795088c359c3c15da954a34dcbf7bcd9
David Ovčačík <david.ovcacik@isobar.com>
2018-11-25 21:13:14 +0100

    cleanup

--- _frontend/admin/js/common.js
+++
@@ -1,8 +0,0 @@
-const swalDefaults = {
-	animation: false,
-	customClass: 'fadeInDown',
-	buttonsStyling: false,
-	confirmButtonClass: 'ui blue button',
-	cancelButtonClass: 'ui red button',
-	reverseButtons: true
-};
--- _frontend/cypress/integration/is-project-working.js
+++
@@ -1,7 +0,0 @@
-const env = require('../../_env');
-
-describe('Lets try to open the project url', function () {
-	it('Visits Home Page', function () {
-		cy.visit(env.PROXY);
-	});
-});
